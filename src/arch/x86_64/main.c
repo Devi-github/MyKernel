386fbe9c250d074eb025f28fa74b0b4037a11094
IgruantyPlay <piuuttri@gmail.com>
2022-05-29 00:04:18 +0200

    Minor fixes

--- src/arch/x86_64/main.c
+++ src/arch/x86_64/main.c
@@ -8,12 +8,12 @@ uint8 digit_ascii_code[] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38
 struct VGAInfo info;
 
 void _cmain() {
+    uint32 seed = __gcca();
+
     info.next_line_index = 1;
     info.g_back_color = BLACK;
     info.g_fore_color = WHITE;
 
-    uint32 seed = __gcca();
-
     init_vga(&info, WHITE, BLACK);
-    print_int(&info, 123123);
+    print_int(&info, seed);
 }
